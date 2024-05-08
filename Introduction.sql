35e927bd1fd78aa1a73e364bdf4c1caefcd4b306
Yagyansh Bagri <77849620+YagyanshB@users.noreply.github.com>
2024-05-08 11:03:41 +0100

    Add files via upload

---
+++ Introduction.sql
@@ -0,0 +1,18 @@
+-- CREATE DATABASE pet_store; 
+
+-- CREATE TABLE cat (
+-- name VARCHAR(140), 
+-- age INT); 
+
+-- CREATE TABLE dog (
+-- name VARCHAR(140), 
+-- age INT);
+
+-- SELECT * FROM cat;
+
+-- show databases;
+
+-- Inserting Values into a Database
+
+INSERT INTO cat(name, age)
+	VALUES("Joanna", "42");
